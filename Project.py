9eab6dfa9ae19d6781b5a9096106d56f71614cad
prakhar96 <prakhar1996@gmail.com>
2016-10-10 12:57:52 +0530

    First Commit

---
+++ Project.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin
+from sklearn.datasets import load_sample_image
+from sklearn.utils import shuffle
+from time import time
+
+n_colors = 64
+
+# Load the Summer Palace photo
+china = load_sample_image("china.jpg")
+
+# Convert to floats instead of the default 8 bits integer coding. Dividing by
+# 255 is important so that plt.imshow behaves works well on float data (need to
+# be in the range [0-1])
+china = np.array(china, dtype=np.float64)/255
+
+# Load Image and transform to a 2D numpy array.
+w, h, d = original_shape = china.shape
+assert d == 3
+image_array = np.reshape(china, (w * h, d))
+
+image_array_sample = shuffle(image_array, random_state=0)[:1000]
+print image_array.size
+print image_array_sample.size
+kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+
+# Get labels for all points
+labels = kmeans.predict(image_array)
+
+
+def recreate_image(codebook, labels, w, h, d):
+    """Recreate the (compressed) image from the code book & labels"""
+    image = np.zeros((w ,h, d))
+    label_idx = 0
+    for i in range(w):
+        for j in range(h):
+            image[i][j] = codebook[labels[label_idx]]
+            label_idx += 1
+    return image
+
+# Display all results, alongside original image
+plt.figure(1)
+plt.clf()
+ax = plt.axes([0, 0, 1, 1])
+plt.axis('off')
+plt.title('Original image (96,615 colors)')
+plt.imshow(china)
+
+plt.figure(2)
+plt.clf()
+ax = plt.axes([0, 0, 1, 1])
+plt.axis('off')
+plt.title('Quantized image (64 colors, K-Means)')
+plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h, d))
+
+plt.show();
