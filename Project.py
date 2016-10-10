00d07742da51ba93e66af3569ecfa395a1482511
prakhar96 <prakhar1996@gmail.com>
2016-10-10 13:15:03 +0530

    Minor Changes

--- Project.py
+++ Project.py
@@ -1,10 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from sklearn.metrics import pairwise_distances_argmin
 from sklearn.datasets import load_sample_image
 from sklearn.utils import shuffle
-from time import time
 
 n_colors = 64
 
